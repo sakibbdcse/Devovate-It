7109c5b4c05155e7eed8fd0fd3204a8f14f33589
sakib <sahadothosain6619@gmail.com>
2025-07-22 22:33:47 +0600

    add who we are secton

---
+++ src/components/whoWeAre/WhoWeAre.tsx
@@ -0,0 +1,15 @@
+import SectionDivider from "../Common/SectionDivider";
+
+const WhoWeAre = () => {
+  return (
+    <>
+      <SectionDivider
+        sectionName="Who We Are"
+        sectionSlug="about our company and working process"
+      />
+      WhoWeAre
+    </>
+  );
+};
+
+export default WhoWeAre;
