31c273c7297924b5adab40e5ddde419637049ec9
sakib <sahadothosain6619@gmail.com>
2025-07-25 23:59:09 +0600

    add services section

---
+++ src/components/ourService/OurService.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import SectionDivider from "../Common/SectionDivider";
+import Services from "../../content/Services";
+import ServiceCard from "../Common/ServiceCard";
+
+const OurService = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  return (
+    <div style={{ backgroundColor: "rgb(25 28 251 / 10%)" }} className="my-4">
+      <SectionDivider
+        sectionName="Our Services"
+        sectionSlug="Share Your idea to Design Any Customized Software and Application"
+      />
+      <div className="container">
+        <div className="row g-3">
+          {Services.map((service, index) => (
+            <div className="col-xl-4 col-md-6">
+              <ServiceCard
+                key={index}
+                index={index}
+                {...service}
+                isActive={activeIndex === index}
+                onClick={setActiveIndex}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurService;
