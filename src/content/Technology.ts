7c9e2f49155994c78da8fdee6b10ae15a9482956
sakib <sahadothosain6619@gmail.com>
2025-08-06 18:25:45 +0600

    add tech

--- src/content/Technology.ts
+++ src/content/Technology.ts
@@ -12,40 +12,40 @@ const Technology = [
     name: "reactNative",
   },
   {
-    logo: "assets/images/technology/python.png",
+    logo: "assets/images/technology/js.png",
+    name: "js",
+  },
+  {
+    logo: "/assets/images/technology/reactJs.png",
     name: "React",
   },
   {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
+    logo: "/assets/images/technology/flutter.png",
+    name: "flutter",
   },
   {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
+    logo: "/assets/images/technology/jQuery.png",
+    name: "jQuery",
   },
   {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
+    logo: "/assets/images/technology/Java.png",
+    name: "Java",
   },
   {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
+    logo: "/assets/images/technology/kotlin.png",
+    name: "kotlin",
   },
   {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
+    logo: "/assets/images/technology/MySql.png",
+    name: "MySql",
   },
   {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
+    logo: "/assets/images/technology/Ios.png",
+    name: "IOS",
   },
   {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
-  },
-  {
-    logo: "/assets/images/technology/python.png",
-    name: "React",
+    logo: "/assets/images/technology/android.png",
+    name: "android",
   },
 ];
 export default Technology;
