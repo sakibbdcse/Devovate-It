0cfbd4d29d74d8418396b16262063a271bd90235
sakib <sahadothosain6619@gmail.com>
2025-08-19 21:01:31 +0600

    add career pages

---
+++ src/pages/Career.tsx
@@ -0,0 +1,12 @@
+import Breadcrumb from "../components/Common/breadcrumb/Breadcrumb";
+
+const Career = () => {
+  return (
+    <>
+      <Breadcrumb pageTitle="Career" pageName="career" pageUrl="/career" />
+      Career
+    </>
+  );
+};
+
+export default Career;
